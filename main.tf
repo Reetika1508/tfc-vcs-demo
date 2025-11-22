a4d56c73d69b2ce5718725fc5ac560de472a4fe0
Ubuntu <ubuntu@ip-172-31-18-90.ec2.internal>
2025-11-22 16:32:15 +0000

    Main.tf created

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  cloud {
+
+    organization = "Reetdev-Demo-Organization"
+
+ 
+
+    workspaces {
+
+      name = "reet-tfc-demo"
+
+    }
+
+  }
+
+}
+
+ 
+
+provider "aws" {
+
+  region = "us-east-1"
+
+}
+
+ 
+
+resource "aws_s3_bucket" "demo" {
+
+  bucket = "tfc-cli-demo-bucket2-1234"
+
+}
